8f722a2a39ed284db3f6e54e13b93492997bb49d
farahsaad505 <farah.saad@esprit.tn>
2025-01-29 10:46:16 +0100

    login page added

--- src/main/java/com/esprit/espritrestau/EspritRestau.java
+++ src/main/java/com/esprit/espritrestau/EspritRestau.java
@@ -5,6 +5,7 @@ import com.esprit.espritrestau.entities.Consommateur;
 import com.esprit.espritrestau.entities.TPA;
 import com.esprit.espritrestau.services.AbonnementService;
 import com.esprit.espritrestau.utils.DatabaseConnection;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -14,33 +15,19 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
 
-public class EspritRestau  {
+public class EspritRestau extends Application {
 
-    //extends Application
-
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(EspritRestau.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(EspritRestau.class.getResource("login_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login!");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
-
-       AbonnementService service = new AbonnementService();
-//        Consommateur m = new Consommateur(2,"Jlassi","Badis",24571421,"12345 ",TPA.ETUDIANT) ;
-//
-//        Abonnement newAbonnement = new Abonnement(152, new java.util.Date(), new java.util.Date(), 1200.0, m.getId());
-//        service.addAbonnement(newAbonnement);
-//
-//        List<Abonnement> allAbonnements = service.getAllAbonnements();
-//        System.out.println("All Abonnements: " + allAbonnements.toString());
-
-        service.consommerRepas(1,1);
-
-
+        launch();
     }
 }
