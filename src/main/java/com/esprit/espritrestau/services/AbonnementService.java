a09540e079b7cbf37418835aa275c72d7ceaf87e
Farah-Saad1 <farah.saad@esprit.tn>
2025-01-31 13:10:35 +0100

    abonnement

--- src/main/java/com/esprit/espritrestau/services/AbonnementService.java
+++ src/main/java/com/esprit/espritrestau/services/AbonnementService.java
@@ -21,11 +21,9 @@
             String insertQuery = "INSERT INTO abonnement (dateDebut, dateFin, solde, idConsomateur) VALUES (?, ?, ?, ?)";
 
             try (PreparedStatement checkStatement = connection.prepareStatement(checkQuery)) {
-                // No need to check ID for insert as it is auto-generated
                 checkStatement.setInt(1, abonnement.getId());
                 ResultSet resultSet = checkStatement.executeQuery();
 
-                // No check for existing ID, just insert the new record
                 try (PreparedStatement insertStatement = connection.prepareStatement(insertQuery)) {
                     insertStatement.setDate(1, new java.sql.Date(abonnement.getDateDebut().getTime()));
                     insertStatement.setDate(2, new java.sql.Date(abonnement.getDateFin().getTime()));
@@ -85,6 +83,7 @@
 
         @Override
         public void updateAbonnement(Abonnement abonnement) {
+            //
             String query = "UPDATE abonnement SET dateDebut = ?, dateFin = ?, solde = ?, idConsomateur = ? WHERE id = ?";
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 statement.setDate(1, new java.sql.Date(abonnement.getDateDebut().getTime()));
